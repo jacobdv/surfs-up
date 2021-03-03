86f49cbbeb5690b87dc61a57a495c2305d33e5e9
Jacob de Vries <mrjacobdevries@gmail.com>
2021-03-02 16:55:41 -0800

    Update error handling for Leap Years.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ###############################################
 # Set-Up + Data Connection
 ###############################################
+# Imports
 from flask import Flask, jsonify
 import numpy as np
 import datetime as dt
@@ -10,8 +11,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+# App and class creations.
 app = Flask(__name__)
-
 engine = create_engine("sqlite:///Resources/hawaii.sqlite",echo=False)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -24,16 +25,16 @@ Station = Base.classes.station
 # Home Page 
 @app.route("/")
 def homepage():
+    # Welcome message and links to available pages, with the url finish listed as well.
     return (
         f"Welcome to the Weather API for Honolulu, Hawaii!"
         f"</br></br>Currently available pages:"
-        f"<ol><a href='/api/v1.0/precipitation'>Precipitation (/api/v1.0/precipitation)</a></ol>"
-        #f"<ol><a href='/api/v1.0/precipitation-grouped'>Precipitation: All Stations Grouped (/api/v1.0/precipitation-grouped)</a></ol>"
-        f"<ol><a href='/api/v1.0/stations'>Stations (/api/v1.0/stations)</a></ol>"
-        f"<ol><a href='/api/v1.0/tobs'>Temperatures (/api/v1.0/tobs)</a></ol>"
+        f"<ul><li><a href='/api/v1.0/precipitation'>Precipitation (/api/v1.0/precipitation)</a></li>"
+        f"<li><a href='/api/v1.0/stations'>Stations (/api/v1.0/stations)</a></li>"
+        f"<li><a href='/api/v1.0/tobs'>Temperatures (/api/v1.0/tobs)</a></li></ul>"
         f"</br></br>User Input Required (Format: YYYY-MM-DD)"
-        f"<ol><a href='/api/v1.0/2010-01-01'>Since Date (/api/v1.0/start_date)</a></ol>"
-        f"<ol><a href='/api/v1.0/2010-01-01/2017-08-23'>Between Dates (/api/v1.0/start_date/end_date)</a></ol>"
+        f"<ul><li><a href='/api/v1.0/2010-01-01'>Since Date (/api/v1.0/start_date)</a></li>"
+        f"<li><a href='/api/v1.0/2010-01-01/2017-08-23'>Between Dates (/api/v1.0/start_date/end_date)</a></li></ul>"
     )
 
 # Precipitation
@@ -53,24 +54,6 @@ def precipitation():
         all_results.append(item_dict)
 
     return jsonify(all_results)
-    
-# # Precipitation - Grouped
-# @app.route("/api/v1.0/precipitation-grouped")
-# def precipitation_grp():
-#     session = Session(engine)
-#     one_year = dt.datetime(2016, 8, 23)
-#     results = session.query(Measurement.date, Measurement.prcp).group_by(Measurement.date).\
-#         order_by(Measurement.date).filter(Measurement.date > one_year).all()
-#     session.close()
-
-#     all_results = []
-#     for item in results:
-#         item_dict = {}
-#         item_dict["Date"] = item[0]
-#         item_dict["Precipitation (inches)"] = item[1]
-#         all_results.append(item_dict)
-
-#     return jsonify(all_results)
 
 # Stations
 @app.route("/api/v1.0/stations")
@@ -117,41 +100,62 @@ def temperatures():
 # Since Date
 @app.route("/api/v1.0/<start_date>")
 def since_date(start_date):
+    # Error Handling - Making sure dates are within proper ranges for year, month and day.
+    # Makes sure the start date is the right length.
     if len(start_date) != 10:
         return (
             "Hmmm, looks like your date is the wrong length. Remember the format is YYYY-MM-DD!"
         )
+    # Makes sure the start year is within the range of years in the data.
     elif int(start_date[0:4]) < 2010 or int(start_date[0:4]) > 2017:
         return (
             "It looks like that year is outside the range of our data! Try a date between January 2010 and August 2017."
             "<br>Make sure to include the month and day too!"
         )
+    # Checks that the given month is a valid month.
     elif int(start_date[5:7]) < 1 or int(start_date[5:7]) > 12:
         return (
             "Hmmm. Make sure you're entering a valid month. Remember the format is YYYY-MM-DD!"
         )
-    elif int(start_date[8:10]) < 1 or int(start_date[8:10]) > 31:
+    # Checks that the given day is greater than 1.
+    elif int(start_date[8:10]) < 1:
         return (
             "Hmmm. Make sure you're entering a valid day. Remember the format is YYYY-MM-DD!"
         )
+    # Clears day values for February, accounting for Leap Years in 2012 and 2016.
+    elif start_date[5:7] == '02':
+        if int(start_date[8:10]) == 29:
+            if int(start_date[0:4]) != 2012 and int(start_date[0:4]) != 2016:  
+                return ("This isn't a Leap Year. Make sure you're using YYYY-MM-DD!")
+        elif int(start_date[8:10]) > 29:
+            return ("This isn't a valid day in February in any year! Make sure you're using YYYY-MM-DD!")
+
+    # Runs standard query.
     try:
+        # Creates a session and queries for the temperature for each date in the selected range.
         session = Session(engine)
         results = session.query(Measurement.tobs).\
             filter(Measurement.date >= start_date).order_by(Measurement.date).all()
         session.close()
 
+        # Creates a new list from the results that doesn't include 'Null' values.
         temp_list = []
         for x in results:
             if x[0] != 'Null':
                 temp_list.append(float(x[0]))
 
+        # Calculates max, min, and average temperature.
         max_temp = max(temp_list)
         min_temp = min(temp_list)
         avg_temp = round(st.mean(temp_list),1)
+
+    # Reminds user of proper date format if there is a value error, though this should be difficult with the above error handling.
     except ValueError:
         return (
             f"It appears you haven't entered a valid date. Please use YYYY-MM-DD format. Thanks!"
         )
+
+    # Returns temperature data since the given start date.
     return (
         f"<u>Temperature Data Since {start_date}</u>"
         f"</br>Max Temperature:    {max_temp}°F"

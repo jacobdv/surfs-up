cbc3920faeefcc0653bf9cf880d7ce291dd96baa
Jacob de Vries <mrjacobdevries@gmail.com>
2021-03-03 12:07:22 -0800

    Comment code. All complete. Couple lingering questions for office hours.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ###############################################
 # Set-Up + Data Connection
 ###############################################
+
 # Imports
 from flask import Flask, jsonify
 import numpy as np
@@ -22,12 +23,14 @@ Station = Base.classes.station
 ############################################### 
 # Static Routes 
 ###############################################
+
 # Home Page 
 @app.route("/")
 def homepage():
+    
     # Welcome message and links to available pages, with the url finish listed as well.
     return (
-        f"Welcome to the Weather API for Honolulu, Hawaii!"
+        f"<title>Home</title>Welcome to the Weather API for Honolulu, Hawaii!"
         f"</br></br>Currently available pages:"
         f"<ul><li><a href='/api/v1.0/precipitation'>Precipitation (/api/v1.0/precipitation)</a></li>"
         f"<li><a href='/api/v1.0/stations'>Stations (/api/v1.0/stations)</a></li>"
@@ -40,12 +43,15 @@ def homepage():
 # Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    
+    # Creates session and queries from the Measurement class the date and precipitation for each station, ordered by date.
     session = Session(engine)
     one_year = dt.datetime(2016, 8, 23)
     results = session.query(Measurement.date, Measurement.prcp).\
         order_by(Measurement.date).filter(Measurement.date > one_year).all()
     session.close()
 
+    # Parses through the results and creates a dictionary to be output.
     all_results = []
     for item in results:
         item_dict = {}
@@ -53,15 +59,19 @@ def precipitation():
         item_dict["Precipitation (inches)"] = item[1]
         all_results.append(item_dict)
 
+    # Returns the dictionary, jsonified.
     return jsonify(all_results)
 
 # Stations
 @app.route("/api/v1.0/stations")
 def stations():
+   
+   # Creates session and queries from the Station class, station name and id, ordered by name.
     session = Session(engine)
     results = session.query(Station.station, Station.name).order_by(Station.name).all()
     session.close()
 
+    # Creates two outputs. One is a list of just the IDs. The other is a dictionary of the name and ID together.
     all_results = []
     station_list = []
     for item in results:
@@ -69,14 +79,16 @@ def stations():
         item_dict["Station ID"] = item[0]
         item_dict["Station Name"] = item[1]
         all_results.append(item_dict)
-        #station_list.append(item[1])
         station_list.append(item[0])
 
+    # Returns first the list and then the dictionary, both jsonified.
     return jsonify(station_list, all_results)
 
 # Temperatures
 @app.route("/api/v1.0/tobs")
 def temperatures():
+    
+    # Creates a session and queries the last year of dates and temperatures only from the most active station (ma).
     session = Session(engine)
     one_year = dt.datetime(2016, 8, 23)
     ma = 'USC00519281' # Most Active Station ID from ipynb Analysis
@@ -85,6 +97,7 @@ def temperatures():
             filter(Measurement.date > one_year).all()
     session.close()
 
+    # Iterates through results to create dictionary of date and temperatures for station (ma).
     all_results = []
     for item in results:
         item_dict = {}
@@ -92,31 +105,38 @@ def temperatures():
         item_dict["Temperature (°F)"] = item[1]
         all_results.append(item_dict)
 
+    # Returns jsonified results.
     return jsonify(all_results)
 
 ############################################### 
 # Dynamic Routes 
 ###############################################
+
 # Since Date
 @app.route("/api/v1.0/<start_date>")
 def since_date(start_date):
+   
     # Error Handling - Making sure dates are within proper ranges for year, month and day.
-    # Makes sure the start date is the right length.
     start_year = int(start_date[0:4])
     start_month = int(start_date[5:7])
     start_day = int(start_date[8:10])
 
+    # Makes sure the start date is the right length.
     if len(start_date) != 10:
         return ("Hmmm, looks like your date is the wrong length. Remember the format is YYYY-MM-DD!")
+    
     # Makes sure the start year is within the range of years in the data.
     elif start_year < 2010 or start_year > 2017:
         return ("It looks like that year is outside the range of our data! Try a date between January 2010 and August 2017.")
+    
     # Checks that the given month is a valid month.
     elif start_month < 1 or start_month > 12:
         return ("Hmmm. Make sure you're entering a valid month. Remember the format is YYYY-MM-DD!")
+    
     # Checks that the given day is greater than 1.
     elif start_day < 1:
         return ("Hmmm. Make sure you're entering a valid day. Remember the format is YYYY-MM-DD!")
+    
     # Clears day values for February, accounting for Leap Years in 2012 and 2016.
     elif start_month == 2:
         if start_day == 29:
@@ -124,18 +144,21 @@ def since_date(start_date):
                 return ("This isn't a Leap Year. Make sure you're using YYYY-MM-DD!")
         elif start_day > 29:
             return ("This isn't a valid day in February in any year! Make sure you're using YYYY-MM-DD!")
+    
     # Clears day values for months with 30 days. (04,06,09,11)
     elif start_month == 4 or start_month == 6 or start_month == 9 or start_month == 11:
         if start_day > 30:
             return ("That isn't a valid day for this month. Make sure you're using YYYY-MM-DD!")
     elif start_day > 31:
         return ("That isn't a valid date in any month. Make sure you're using YYYY-MM-DD!")
+    
     # Deals with the fact that the dataset ends halfway through 2017.
     elif start_year == 2017 and ((start_month == 8 and start_day > 23) or start_month > 8):
         return ("Sorry. The last day of data in this dataset is August 23rd, 2017.")
 
     # Runs standard query.
     try:
+        
         # Creates a session and queries for the temperature for each date in the selected range.
         session = Session(engine)
         results = session.query(Measurement.tobs).\
@@ -159,7 +182,7 @@ def since_date(start_date):
 
     # Returns temperature data since the given start date.
     return (
-        f"<u>Temperature Data Since {start_date}</u>"
+        f"<title>{start_date}</title><u>Temperature Data Since {start_date}</u>"
         f"</br>Max Temperature:    {max_temp}°F"
         f"</br>Min Temperature:    {min_temp}°F"
         f"</br>Average Temperature:    {avg_temp}°F"
@@ -168,25 +191,31 @@ def since_date(start_date):
 # Between Date
 @app.route("/api/v1.0/<start_date>/<end_date>")
 def between_dates(start_date,end_date):
+    
+    # Pulls year, month, and day values for the input start and end date.
     start_year = int(start_date[0:4])
     start_month = int(start_date[5:7])
     start_day = int(start_date[8:10])
-
     end_year = int(end_date[0:4])
     end_month = int(end_date[5:7])
     end_day = int(end_date[8:10])
 
+    # Makes sure the input start date is the right length.
     if len(start_date) != 10:
         return ("Hmmm, looks like your start date is the wrong length. Remember the format is YYYY-MM-DD!")
+   
     # Makes sure the start year is within the range of years in the data.
     elif start_year < 2010 or start_year > 2017:
         return ("It looks like that year is outside the range of our data! Try a date between January 2010 and August 2017.")
+    
     # Checks that the given month is a valid month.
     elif start_month < 1 or start_month > 12:
         return ("Hmmm. Make sure you're entering a valid month. Remember the format is YYYY-MM-DD!")
+    
     # Checks that the given day is greater than 1.
     elif start_day < 1:
         return ("Hmmm. Make sure you're entering a valid day. Remember the format is YYYY-MM-DD!")
+    
     # Clears day values for February, accounting for Leap Years in 2012 and 2016.
     elif start_month == 2:
         if start_day == 29:
@@ -194,30 +223,34 @@ def between_dates(start_date,end_date):
                 return ("This isn't a Leap Year. Make sure you're using YYYY-MM-DD!")
         elif start_day > 29:
             return ("This isn't a valid day in February in any year! Make sure you're using YYYY-MM-DD!")
+    
     # Clears day values for months with 30 days. (04,06,09,11)
     elif start_month == 4 or start_month == 6 or start_month == 9 or start_month == 11:
         if start_day > 30:
             return ("That isn't a valid day for this month. Make sure you're using YYYY-MM-DD!")
     elif start_day > 31:
         return ("That isn't a valid date in any month. Make sure you're using YYYY-MM-DD!")
+    
     # Deals with the fact that the dataset ends halfway through 2017.
     elif start_year == 2017 and ((start_month == 8 and start_day > 23) or start_month > 8):
         return ("Sorry. The last day of data in this dataset is August 23rd, 2017.")
-
-#################################
-
-
+    
+    # Makes sure the input end date is the right length.
     if len(end_date) != 10:
         return ("Hmmm, looks like your end date is the wrong length. Remember the format is YYYY-MM-DD!")
+    
     # Makes sure the start year is within the range of years in the data.
     elif end_year < 2010 or end_year > 2017:
         return ("It looks like that year is outside the range of our data! Try a date between January 2010 and August 2017.")
+    
     # Checks that the given month is a valid month.
     elif end_month < 1 or end_month > 12:
         return ("Hmmm. Make sure you're entering a valid month. Remember the format is YYYY-MM-DD!")
+    
     # Checks that the given day is greater than 1.
     elif end_day < 1:
         return ("Hmmm. Make sure you're entering a valid day. Remember the format is YYYY-MM-DD!")
+    
     # Clears day values for February, accounting for Leap Years in 2012 and 2016.
     elif end_month == 2:
         if end_day == 29:
@@ -225,32 +258,41 @@ def between_dates(start_date,end_date):
                 return ("This isn't a Leap Year. Make sure you're using YYYY-MM-DD!")
         elif end_day > 29:
             return ("This isn't a valid day in February in any year! Make sure you're using YYYY-MM-DD!")
+    
     # Clears day values for months with 30 days. (04,06,09,11)
     elif end_month == 4 or end_month == 6 or end_month == 9 or end_month == 11:
         if end_day > 30:
             return ("That isn't a valid day for this month. Make sure you're using YYYY-MM-DD!")
     elif end_day > 31:
         return ("That isn't a valid date in any month. Make sure you're using YYYY-MM-DD!")
+
     # Deals with the fact that the dataset ends halfway through 2017.
     elif end_year == 2017 and ((end_month == 8 and end_day > 23) or end_month > 8):
         return ("Sorry. The last day of data in this dataset is August 23rd, 2017.")
-##################################
 
+    # Tries standard query but accounts for ValueError.
     try:
+
+        # Creates session and queries for temperature between given dates.
         session = Session(engine)
         results = session.query(Measurement.tobs).\
             filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).\
                 order_by(Measurement.date).all()
         session.close()
 
+        # Creates new list without 'Null' temperatures.
         temp_list = []
         for x in results:
             if x[0] != 'Null':
                 temp_list.append(float(x[0]))
 
+        # Calculates max, min, and average temperature from the list.
         max_temp = max(temp_list)
         min_temp = min(temp_list)
         avg_temp = round(st.mean(temp_list),1)
+
+    # Returns error message if the start date is after the end date in any fashion or other error message if they somehow
+    #   entered a date that made it through all the error handling.
     except ValueError:
         if (int(start_date[0:4]) > int(end_date[0:4])) or\
         ((start_date[0:4] == end_date[0:4]) and (int(start_date[5:7]) > int(end_date[5:7]))) or\
@@ -258,12 +300,15 @@ def between_dates(start_date,end_date):
             return ("Make sure your end date is after your start date. Remember the format is YYYY-MM-DD!")
         else:
             return ("It appears you haven't entered a valid date. Please use YYYY-MM-DD format. Thanks!")
+    
+    # Returns temperature data - finally!
     return (
-        f"<u>Temperature Data Between {start_date} and {end_date}</u>"
+        f"<title>{start_date} to {end_date}</title><u>Temperature Data Between {start_date} and {end_date}</u>"
         f"</br>Max Temperature:    {max_temp}°F"
         f"</br>Min Temperature:    {min_temp}°F"
         f"</br>Average Temperature:    {avg_temp}°F"
     )
 
+# Runs app and debugger.
 if __name__ == "__main__":
     app.run(debug=True)
